6569ff88378631699de18666042374a789bf5dd0
lujanfernaud <lujanfernaud@gmail.com>
2017-11-26 08:47:40 +0000

    Refactor Tipper#total to actually use temps

--- lib/tipper.rb
+++ lib/tipper.rb
@@ -8,22 +8,14 @@ class Tipper
   end
 
   def total
-    amount_with_tax - amount_with_discount + amount_with_tip
+    tax      = amount * TAX
+    discount = amount * (discount_percentage / 100.0)
+    tip      = amount * (tip_percentage / 100.0)
+
+    amount + tax - discount + tip
   end
 
   private
 
   attr_reader :amount, :discount_percentage, :tip_percentage
-
-  def amount_with_tax
-    amount + (amount * TAX)
-  end
-
-  def amount_with_discount
-    amount * (discount_percentage / 100.0)
-  end
-
-  def amount_with_tip
-    amount * (tip_percentage / 100.0)
-  end
 end
